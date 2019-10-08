1c7a6b5949f3c18e668acd1816bd6062fffaef69
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-08 09:12:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,14 +2,14 @@ class Anagram
   attr_accessor :word
 
   MATCHES = []
-  
+
   def initialize(word)
     @word = word
   end
 
   def match(word)
     anagram = Array.new
-    word.split("").Array.new
+    word.split("").all? == word 
 
 
   end
