095b63a671d56efb5b44a4192c98d73d798e34fc
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-08 09:11:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,15 @@
 class Anagram
   attr_accessor :word
 
+  MATCHES = []
+  
   def initialize(word)
     @word = word
   end
 
   def match(word)
     anagram = Array.new
+    word.split("").Array.new
 
 
   end
